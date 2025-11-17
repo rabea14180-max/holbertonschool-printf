c3a063bf47fb9e06830d6b8fe0b3db5b002c9031
rabea14180-max <rabea14180@gmail.com>
2025-11-17 15:01:55 +0000

    c0

--- printf.c
+++ printf.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include <stdarg.h>
-
+/**
+ * _printf - produces output according to a format
+ * @format: a character string containing zero or more directives
+ *
+ * Handles the following conversion specifiers:
+ *  c: prints a character
+ *  s: prints a string
+ *  %: prints a percent sign
+ *
+ * Return: the number of characters printed (excluding the null byte)
+ *         or -1 if format is NULL
+ */
 int _printf(const char *format, ...)
 {
 va_list args;
