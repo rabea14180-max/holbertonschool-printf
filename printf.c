a4c69a9b418043ed6168050cc4bad0bd51831725
rabea14180-max <rabea14180@gmail.com>
2025-11-17 14:44:39 +0000

    c0

--- printf.c
+++ printf.c
@@ -1,73 +1,42 @@
 #include "main.h"
 #include <stdarg.h>
-
-/**
- * _printf - produces output according to a format
- * @format: character string containing directives
- *
- * Return: number of characters printed
- */
 int _printf(const char *format, ...)
 {
 va_list args;
-int i = 0, count = 0;
-
-if (format == NULL)
+int i = 0, j, count = 0;
+char *str;
+if (!format)
 return (-1);
-
 va_start(args, format);
-
 while (format[i])
 {
 if (format[i] != '%')
-{
-_putchar(format[i]);
-count++;
-}
+count += _putchar(format[i]);
 else
 {
 i++;
-
-if (format[i] == '\0')
-return (-1);
-
+if (!format[i])
+return (-1)
 if (format[i] == 'c')
-{
-char c = va_arg(args, int);
-_putchar(c);
-count++;
-}
+count += _putchar(va_arg(args, int));
 else if (format[i] == 's')
 {
-char *str = va_arg(args, char *);
-int j = 0;
-
-if (str == NULL)
+str = va_arg(args, char *);
+if (!str)
 str = "(null)";
-
-while (str[j])
-{
-_putchar(str[j]);
-count++;
-j++;
-}
+for (j = 0; str[j]; j++)
+count += _putchar(str[j]);
 }
 else if (format[i] == '%')
-{
-_putchar('%');
-count++;
-}
+count += _putchar('%');
 else
 {
-
-_putchar('%');
-_putchar(format[i]);
-count += 2;
+count += _putchar('%');
+count += _putchar(format[i]);
 }
 }
 i++;
 }
-
 va_end(args);
 return (count);
 }
